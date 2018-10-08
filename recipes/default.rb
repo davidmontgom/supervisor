99d8daf5cdf045a072b0c83ef168c7529f60340b
David Montgomery <davidmontgomery@gmail.com>
2018-10-08 22:57:28 +0800

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -35,21 +35,29 @@ python_package "setuptools" do
 end
 =end
 
+
+package "python3-pip" do
+  action :install
+end
+
+
+bash "install_pip" do
+  cwd "/tmp/"
+  code <<-EOH
+  wget https://bootstrap.pypa.io/get-pip.py
+  /usr/bin/python2.7 get-pip.py
+EOH
+  creates "#{Chef::Config[:file_cache_path]}/get-pip.lock"
+  not_if {File.exists?("#{Chef::Config[:file_cache_path]}/get-pip.lock")}
+end
+
 bash "install_supervisor_setuptools" do
   code <<-EOH
   	pip install --upgrade setuptools
   EOH
 end
 
-bash "install_pip" do
-  cwd "/tmp/"
-  code <<-EOH
-  wget https://bootstrap.pypa.io/get-pip.py
-  /usr/bin/python get-pip.py
-EOH
-  creates "#{Chef::Config[:file_cache_path]}/get-pip.lock"
-  not_if {File.exists?("#{Chef::Config[:file_cache_path]}/get-pip.lock")}
-end
+
 
 =begin
 package "python-pip" do
